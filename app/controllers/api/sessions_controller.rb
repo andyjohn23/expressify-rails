f44806248d3af09c72501b0b2de76fef2046f2f0
andyjohn23 <andrewnick906@gmail.com>
2022-08-04 12:22:19 +0300

    finished creating the sessions controller create and destroy methods

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,17 @@
 class Api::SessionsController < ApplicationController
+    def create
+        user = User.find_by(email: params[:email])
+
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id 
+            render json: user, include: ['posts'], status: :created
+        else
+            render json: { errors: ["Either username or password is invalid!"] }, status: :unauthorized
+        end
+    end
+
+    def destroy 
+        session.delete :user_id 
+        head :no_content
+    end
 end
