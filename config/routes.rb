34f1124c8503cd7c5c1c2b072d58caa54562253e
andyjohn23 <andrewnick906@gmail.com>
2022-08-04 12:19:12 +0300

    created the necessary routes for user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    resources :users, only: [:index, :destroy, :update]
+    get '/register', to: "users#create"
+    get '/me', to: "users#show"
+  end
 end
