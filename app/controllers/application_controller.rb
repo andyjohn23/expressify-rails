91c0ab55a08dd4a52b9408e8eb7f77838a39b074
andyjohn23 <andrewnick906@gmail.com>
2022-10-03 13:33:57 +0300

    revamped the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,46 @@
 class ApplicationController < ActionController::API
-    include ActionController::Cookies
-    rescue_from ActiveRecord::RecordInvalid, with: :invalid_record
-    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
-    
-    before_action :authorize 
-  
-    private
-    
-    def authorize 
-      @current_user = User.find_by(id: session[:user_id])
-    end
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
-    def record_not_found(errors)
-      render json: errors.message, status: :not_found
-    end
-  
-    def invalid_record(invalid)
-      render json: invalid.records.errors, status: :unprocessable_entity
-    end
+  private
+
+  def render_unprocessable_entity_response(exception)
+      render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
+  # payload token encode
+  def encoded_token(payload)
+      JWT.encode(payload, 'expressifyApiToken')
+  end
+
+  def authorized_user
+      if decoded_token
+          user_id = decoded_token[0]['id']
+          @user = User.find(user_id)
+          render json: { errors: ["You are not authorized"] }, status: :unauthorized unless @user
+      else
+          render json: { errors: ["You are not authorized"] }, status: :unauthorized
+      end
+  end
+
+  # Authorization header check
+  def auth_header
+      request.headers['Authorization']
+  end
+
+  # decoding token bearer
+  def decoded_token
+      if auth_header
+          token = auth_header.split(' ')[1]
+      begin
+          JWT.decode(token, 'expressifyApiToken', true, algorithm: 'HS256')
+      rescue JWT::DecodeError
+          nil
+      end
+      end
+  end
+
+  def record_not_found
+      render json: { errors: ['Record not found'] }
+  end
 end
